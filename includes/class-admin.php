12a76d4e14c92035d2690465944a57e1572b8334
Govind Kumar <unavailable2010@gmail.com>
2016-11-28 16:02:12 +0530

    version 1.1 code update.

---
+++ includes/class-admin.php
@@ -0,0 +1,120 @@
+<?php
+
+// Avoid direct calls to this file
+if ( ! function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+class VisualTextAdmin {
+
+	protected $overlayColor = '#fff';
+	protected $autop 		= false;
+	protected $dragndrop 	= false;
+
+	public function __construct() {
+
+		add_action( 'admin_menu', array( $this, 'veadminoptions' ) );
+		add_action( 'admin_init', array( $this, 'registerPluginSetting' ) );
+		add_action( 'admin_head', array( $this, 'veChangeStyle' ) );
+		add_shortcode( 'addcode',array( $this, 'addcode_callback' ) );
+
+		if ( get_option( 'autop' ) == 'on' ) {
+			$this->autop = true;
+		}
+
+		if ( get_option( 'draganddrop' ) == 'on' ) {
+			$this->dragndrop = true;
+		}
+
+		if ( get_option( 'overlaycolor' ) != '' ) {
+			$this->overlayColor = get_option( 'overlaycolor' );
+		}
+	}
+
+	public function addcode_callback( $atts ) {
+		echo 'fdsfdsf';
+	}
+
+	public function veChangeStyle() {
+
+		?>
+		<style>
+		#visual-editor-overlay{
+		background: <?php echo $this->overlayColor;?>;
+		}
+		</style>
+		<?php
+	}
+
+	public function registerJSCSS() {
+		wp_enqueue_style( 'wp-color-picker' );
+		wp_enqueue_style( 'wp-color-picker' );
+		wp_enqueue_script( 'wp-color-picker-alpha', WPVE_URL . '/js/wp-color-picker-alpha.js', array( 'wp-color-picker' ), '1.2.2' );
+	}
+
+	public function registerPluginSetting() {
+		register_setting( 'wpvisualsettings', 'editorheight' );
+		register_setting( 'wpvisualsettings', 'overlaycolor' );
+		register_setting( 'wpvisualsettings', 'autop' );
+		register_setting( 'wpvisualsettings', 'mediabuttons' );
+		register_setting( 'wpvisualsettings', 'dragndrop' );
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'registerJSCSS' ) );
+	}
+
+	public function veadminoptions() {
+		add_menu_page( 'WP Text Widget Settings', 'WP Visual Text ', 'manage_options', 'wptextwidgetsettings', array( $this, 'veTextWidgetSetting' ), WPVE_URL . 'img/wpve-icon.png' );
+	}
+
+	public function veTextWidgetSetting() {
+		?>
+		<h2><?php echo __('General Options',TEXTDOMAIN);?></h2>
+		<hr/>
+		<form action="options.php" method="post">
+		<?php
+		settings_fields( 'wpvisualsettings' );
+		do_settings_sections( 'wpvisualsettings' );
+		?>
+		<table class="form-table">
+		<tr valign="top">
+		<th scope="row"><?php echo __( 'Visual Editor height',TEXTDOMAIN );?></th>
+		<td><input type="number" min="5" max="600" name="editorheight" value="<?php echo esc_attr( get_option( 'editorheight' ) );?>"/><?php echo __( 'px',TEXTDOMAIN );?><br/><span class="description"><?php echo __( 'Number of textarea rows',TEXTDOMAIN );?> </span></td>
+		</tr>
+		<tr>
+		<th scope="row"><?php echo __( 'Overlay Color',TEXTDOMAIN );?> </th>
+		<td><input type="text"  name="overlaycolor" data-alpha="true" id="overlaycolor" value="<?php echo esc_attr( get_option( 'overlaycolor' ) );?>"/><br/><span class="description"><?php echo __( 'Lightbox overlay color',TEXTDOMAIN );?> </span></td>
+		</tr>   
+		<tr>
+		<th scope="row">
+			<?php echo __( 'Auto Paragraph',TEXTDOMAIN );?>
+		</th>
+		<td>
+			<label for="autop"><input type="checkbox" id="autop"  name="autop" data-alpha="true" id="autop" <?php checked( esc_attr( get_option( 'autop' ) ), 'on', true );?> /><?php echo __( 'Enable',TEXTDOMAIN );?></label>
+			<br/><span class="description"><?php echo __( 'Add paragraph (&lt;p&gt;&lt;/p&gt;) to content automatically.',TEXTDOMAIN );?></span>
+		</td>
+		</tr>
+		<tr>
+		<th scope="row">    
+			<?php echo __( 'Media buttons',TEXTDOMAIN );?>
+		</th>
+		<td>
+			<label for="mediabuttons"><input type="checkbox" id="mediabuttons"  name="mediabuttons" id="mediabuttons" <?php checked( esc_attr( get_option( 'mediabuttons' ) ), 'on', true );?> /><?php echo __( 'Enable',TEXTDOMAIN );?></label><br/><span class="description"><?php echo __( 'Whether to display media insert/upload buttons',TEXTDOMAIN );?></span>
+		</td>
+		</tr>
+		<tr>
+		<th scope="row">    
+			<?php echo __( 'Drag & Drop Upload',TEXTDOMAIN );?>
+		</th>
+		<td>
+			<label for="dragndrop"><input type="checkbox" id="dragndrop"  name="dragndrop" <?php checked( esc_attr( get_option( 'dragndrop' ) ), 'on', true );?> /><?php echo __( 'Enable',TEXTDOMAIN );?></label><Br/><span class="description"><?php echo __( 'Enable Drag & Drop Upload Support',TEXTDOMAIN );?></span>
+		</td>
+		</tr>
+		</table>
+		<?php submit_button( 'Save Settings' );?>
+		</form>     
+		<?php
+	}
+}
+
+new VisualTextAdmin();
---
+++ includes/class-widget.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Adds VisualTextEditorWidget widget.
+ */
+
+
+class VisualTextEditorWidget extends WP_Widget {
+
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+
+		$widget_ops = apply_filters(
+			'visual-text-editor',
+			array(
+				'classname'     => 'widget_text',
+				'description'   => __( 'This Widget allow you to add html content using visual editor inside Default WP Text Widget.', 'visual-text-editor' ),
+			)
+		);
+
+		parent::__construct(
+			'text',
+			__( 'Text', 'visual-text-editor' ),
+			$widget_ops
+		);
+	} // END __construct()
+
+	public function widget( $args, $instance ) {
+
+		extract( $args );
+
+		$title          = apply_filters( 'widget_title', $instance['title'] );
+		$content        = apply_filters( 'visual_editor_content', $instance['text'] );
+		echo $before_widget;
+		echo $before_title . $title . $after_title;
+		$final_content = $content;
+		/*
+            if (strpos($content, '<' . '?') !== false) {
+                ob_start();
+                eval('?' . '>' . $content);
+                $final_content = ob_get_clean();
+            }
+        */
+		echo $final_content;
+		echo $after_widget;
+	} // END widget()
+
+
+	public function form( $instance ) {
+
+		if ( isset( $instance['title'] ) ) {
+			$title = $instance['title'];
+		} else {
+			$title = __( 'New title', 'visual-text-editor' );
+		}
+
+		if ( isset( $instance['text'] ) ) {
+			$text = $instance['text'];
+		} else {
+			$text = '';
+		}
+
+		$output_title = ( isset( $instance['output_title'] ) && $instance['output_title'] == '1' ? true : false );
+
+		?>
+		<input type="hidden" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>" value="<?php echo esc_attr( $text ); ?>">
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'visual-text-editor' ); ?>:</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<p>
+			<a href="javascript:VisualTextEditorWidget.showEditor('<?php echo $this->get_field_id( 'text' ); ?>');" class="button"><?php _e( 'Open Visual Editor', TEXTDOMAIN ) ?></a> 
+		</p>
+
+		<?php
+	} // END form()
+
+
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = array();
+		$instance['title']      = ( ! empty( $new_instance['title'] ) ? strip_tags( $new_instance['title'] ) : '' );
+		$instance['text']       = ( ! empty( $new_instance['text'] ) ? $new_instance['text'] : '' );
+
+		do_action( 'visual_editor_update', $new_instance, $instance );
+
+		return apply_filters( 'visual_editor_instance_update', $instance, $new_instance );
+	} // END update()
+} // END class VisualTextEditorWidget
+
